cad734fd1d17288e549f5e0a51046bf28e185229
margaret <margaret@responsive-software.com>
2018-03-12 14:34:42 -0700

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { FavoritesPage } from "../pages/favorites/favorites";
+import { LibraryPage } from "../pages/library/library";
+import { QuotesPage } from "../pages/quotes/quotes";
+import { QuotePage } from "../pages/quote/quote";
+import { SettingsPage } from "../pages/settings/settings";
+import { TabsPage } from "../pages/tabs/tabs";
+import { QuotesService } from "../services/quotes";
+import { SettingsService } from "../services/settings";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage,
+    TabsPage
   ],
   imports: [
-    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage,
+    TabsPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    QuotesService,
+    SettingsService
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
